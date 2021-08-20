357070cf7e3087892d2dc505e7add1d4c5903e3a
Dalibur <al2172002801@azc.uam.mx>
2021-08-19 22:11:10 -0500

    Pigpio
    Ejemplo con la bliblioteca Pigpio en C y la biblioteca misma

--- LedBasico/LedBasico.c
+++ LedBasico/LedBasico.c
@@ -1,16 +1,31 @@
-#include <wiringPi.h>
+/*
+    Autor: Jorge Isur Balderas Ramírez
+    Fecha: 7 de agosto de 2021
+    Descripción: Manipula los pines GPIO para encender un led, compilado en una 
+    computadora con arquitectura x86_64 para un dispositivo arm64 raspberry pi 3 model B+
+    GPIO4--->Pin7
+*/
+#include <stdio.h>
+#include <pigpio.h>
+#include <time.h>
+#include <unistd.h>
 
-int main (void)
+//Definición del PIN de acuerdo al broadcom
+#define LED 4
+
+int main(void)
 {
-    wiringPiSetup();
-    pinMode(0, OUTPUT);
-    int i = 0;
-    for (i = 0;i<10;i++)
+    printf("Hola samsung innovation campus!\n");
+    if (gpioInitialise()<0) return 1;
+    gpioSetMode(LED, PI_OUTPUT);
+    for (int i = 0; i < 10; i++)
     {
-        digitalWrite(0, HIGH);
-        delay(500);
-        digitalWrite(0, LOW);
-        delay(500);
+        gpioWrite(LED, 1); //on
+        printf("Hola\n");
+        sleep(1);  //seconds
+        gpioWrite(LED,0);  //off
+        printf("Adios\n");
+        sleep(1);
     }
-    return 0 ;
+    gpioTerminate();
 }
